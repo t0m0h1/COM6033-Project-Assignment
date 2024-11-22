762f4dff851ccfe1eb8c34a08cfc9dcfc89e7899
t0m0h1 <thomashathawayhogg@gmail.com>
2024-11-22 12:58:12 +0000

    update

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ import joblib
 app = Flask(__name__)
 CORS(app)  
 
-# Load the model and vectorizer
+# Load the model and vectoriser to use the model in app
 model = joblib.load('model.pkl')
-vectorizer = joblib.load('vectoriser.pkl')
+vectoriser = joblib.load('vectoriser.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     text = data.get('text', '')
-    text_vec = vectorizer.transform([text])
+    text_vec = vectoriser.transform([text])
     prediction = model.predict(text_vec)
     return jsonify({'sentiment': prediction[0]})
 

9ba695011262175dff44ca96af72863bf0fa92ee
t0m0h1 <thomashathawayhogg@gmail.com>
2024-11-22 00:30:51 +0000

    1st working version with cross origin requests

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 
 app = Flask(__name__)
+CORS(app)  
 
-# Load the saved model and vectorizer
+# Load the model and vectorizer
 model = joblib.load('model.pkl')
-vectoriser = joblib.load('vectoriser.pkl')
+vectorizer = joblib.load('vectoriser.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    text = data.get('text', '')  # Extract text from request
-    text_vec = vectoriser.transform([text])  # Transform input text
-    prediction = model.predict(text_vec)  # Predict sentiment
+    text = data.get('text', '')
+    text_vec = vectorizer.transform([text])
+    prediction = model.predict(text_vec)
     return jsonify({'sentiment': prediction[0]})
 
 if __name__ == '__main__':
